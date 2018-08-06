cf935a2de55789b54b67cf73bd7e44ae1092d6d7
AppleConnoiseur <chjeesy@gmail.com>
2018-08-06 15:25:27 +0200

    Adds nerve stapling. War crimes ahoy \o/.

--- DefOfs/QEThoughtDefOf.cs
+++ DefOfs/QEThoughtDefOf.cs
@@ -11,5 +11,9 @@ namespace QEthics
     public static class QEThoughtDefOf
     {
         public static ThoughtDef QE_VatGrownCloneConfusion;
+        public static ThoughtDef QE_RecentlyNerveStapled;
+        public static ThoughtDef QE_NerveStapledPawn;
+        public static ThoughtDef QE_NerveStapledColonist;
+        public static ThoughtDef QE_NerveStapledMe;
     }
 }
---
+++ Workers/RecipeWorker_NerveStapling.cs
@@ -0,0 +1,49 @@
+﻿using RimWorld;
+using Verse;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace QEthics
+{
+    public class RecipeWorker_NerveStapling : Recipe_InstallImplant
+    {
+        public override void ApplyOnPawn(Pawn pawn, BodyPartRecord part, Pawn billDoer, List<Thing> ingredients, Bill bill)
+        {
+            base.ApplyOnPawn(pawn, part, billDoer, ingredients, bill);
+
+            //Check if the Hediff got applied.
+            if (!pawn.health.hediffSet.HasHediff(recipe.addsHediff))
+            {
+                return;
+            }
+
+            //Convert to billDoer faction.
+            bool isColonist = pawn.Faction == billDoer.Faction;
+            if (pawn.Faction != billDoer.Faction)
+            {
+                pawn.SetFaction(billDoer.Faction, billDoer);
+            }
+
+            //Apply thoughts.
+            pawn.needs.mood.thoughts.memories.TryGainMemory(QEThoughtDefOf.QE_RecentlyNerveStapled);
+            pawn.needs.mood.thoughts.memories.TryGainMemory(QEThoughtDefOf.QE_NerveStapledMe, billDoer);
+
+            foreach (Pawn thoughtReciever in PawnsFinder.AllMapsCaravansAndTravelingTransportPods_Alive_FreeColonistsAndPrisoners)
+            {
+                if(thoughtReciever != pawn)
+                {
+                    if(isColonist)
+                    {
+                        thoughtReciever.needs.mood.thoughts.memories.TryGainMemory(QEThoughtDefOf.QE_NerveStapledColonist);
+                    }
+                    else
+                    {
+                        thoughtReciever.needs.mood.thoughts.memories.TryGainMemory(QEThoughtDefOf.QE_NerveStapledPawn);
+                    }
+                }
+            }
+        }
+    }
+}
