83b48cd42466387e5fe1727241608ebcdef1b876
KongMD <KongMDSteam@gmail.com>
2019-05-19 18:17:22 -0400

    Fix #20 - Family relationships created for clones

---
+++ QEE/Workers/PawnRelationWorker_ChildCloneCheck.cs
@@ -0,0 +1,23 @@
+﻿using RimWorld;
+using Verse;
+
+namespace QEthics
+{
+    public class PawnRelationWorker_ChildCloneCheck : PawnRelationWorker_Child
+    {
+
+        public override void CreateRelation(Pawn generated, Pawn other, ref PawnGenerationRequest request)
+        {
+            //check if 'other' pawn is a Clone. If so, prevent the relation from happening
+            if (other.health.hediffSet.HasHediff(QEHediffDefOf.QE_CloneStatus))
+            {
+                return;
+            }
+            else
+            {
+                base.CreateRelation(generated, other, ref request);
+            }
+        }
+
+    }
+}
