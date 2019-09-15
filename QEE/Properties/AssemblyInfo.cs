6a63b33a390bb40497c7fa6c010b1c0c94342b30
KongMD <KongMDSteam@gmail.com>
2019-09-15 18:05:36 -0400

    Bump version to 1.0.2

--- QEE/Properties/AssemblyInfo.cs
+++ QEE/Properties/AssemblyInfo.cs
@@ -32,5 +32,8 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
+
+// **AssemblyVersion will never be changed for this project, unless there's a complete overhaul that breaks things (I don't plan on that).**
+// Communication will be made in advance, if that happens.
 [assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.2.0")]
